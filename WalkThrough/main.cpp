4eaa997d30c334e51ec20ee03af3420e8eaa00b1
davidsackstein <davids@codeprecise.com>
2019-06-11 01:17:42 +0300

    Create the Test class

--- WalkThrough/main.cpp
+++ WalkThrough/main.cpp
@@ -6,154 +6,166 @@
 #include <unistd.h>
 #include <time.h>
 
-const int size = 1000;
-
-Queue* sourceQueue = NULL;
-Queue* evenQueue = NULL;
-Queue* oddQueue = NULL;
-
-volatile int shouldExit = 0;
-
-bool Initialize()
+class Test
 {
-    sourceQueue = new Queue(size, sizeof(int));
-    if (!sourceQueue) {
-        return false;
-    }
+public:
 
-    evenQueue = new Queue(size / 2, sizeof(int));
-    if (!evenQueue) {
-        return false;
-    }
-    
-    oddQueue = new Queue(size / 2, sizeof(int));
-    if (!oddQueue) {
-        return false;
-    }
+    const int size = 1000;
 
-    return true;
-}
+    Queue* sourceQueue = NULL;
+    Queue* evenQueue = NULL;
+    Queue* oddQueue = NULL;
 
-void Release()
-{
-    if (sourceQueue) {
-        delete sourceQueue;
-    }
+    volatile int shouldExit = 0;
 
-    if (evenQueue) {
-        delete evenQueue;
-    }
-    
-    if (oddQueue) {
-        delete oddQueue;
-    }
-}
-
-void* ProducerMethod(void* arg)
-{
-    (void) arg;
-    for (int i = 0; (i < size) && !shouldExit; i++)
+    Test()
     {
-        int rand = clock() % 7;
-        usleep(rand * 1000);
-        sourceQueue->Enqueue(&i);
-    }
-
-    return NULL;
-}
-
-void* ConsumerMethod(void* arg)
-{
-    (void)arg;
-    while (! shouldExit)
-    {
-        int rand = clock() % 7;
-        usleep(rand * 1000);
-
-        int value;
-        int gotValue = sourceQueue->Dequeue(&value);
-        if (gotValue)
-        {
-            if (value % 2 == 0)
-            {
-                evenQueue->Enqueue(&value);
-            }
-            else
-            {
-                oddQueue->Enqueue(&value);
-            }
+        sourceQueue = new Queue(size, sizeof(int));
+        if (!sourceQueue) {
+            return;
         }
-        else if (evenQueue->GetSize() + oddQueue->GetSize() == size)
-        {
-            break;
+
+        evenQueue = new Queue(size / 2, sizeof(int));
+        if (!evenQueue) {
+            return;
+        }
+
+        oddQueue = new Queue(size / 2, sizeof(int));
+        if (!oddQueue) {
+            return;
         }
     }
 
-    return NULL;
-}
-
-void RunThreads()
-{
-    Thread producerThread(ProducerMethod, NULL);
-    Thread consumer1Thread(ConsumerMethod, NULL);
-    Thread consumer2Thread(ConsumerMethod, NULL);
-
-    producerThread.Join();
-    consumer1Thread.Join();
-    consumer2Thread.Join();
-}
-
-bool IsPassed()
-{
-    int evenSize = evenQueue->GetSize();
-    int oddSize = oddQueue->GetSize();
-
-    bool passed =
-        evenSize == (size_t)(size / 2) &&
-        oddSize == (size_t)(size / 2);
-
-    if (!passed)
+    ~Test()
     {
-        return false;
+        if (sourceQueue) {
+            delete sourceQueue;
+        }
+     
+        if (evenQueue) {
+            delete evenQueue;
+        }
+
+        if (sourceQueue) {
+            delete oddQueue;
+        }
     }
 
-    int sum = 0;
-    int value = 0;
-
-    for (int i = 0; i < evenSize; i++)
+    bool IsOk()
     {
-        evenQueue->Dequeue(&value);
-        sum += value;
+        return sourceQueue != NULL;
     }
 
-    for (int i = 0; i < oddSize; i++)
+    void ProducerMethod()
     {
-        oddQueue->Dequeue(&value);
-        sum += value;
+        for (int i = 0; (i < size) && !shouldExit; i++)
+        {
+            int rand = clock() % 7;
+            usleep(rand * 1000);
+            sourceQueue->Enqueue(&i);
+        }
     }
 
-    if (sum != (0 + size - 1) * size / 2) {
-        return false;
+    void ConsumerMethod()
+    {
+        while (!shouldExit)
+        {
+            int rand = clock() % 7;
+            usleep(rand * 1000);
+
+            int value;
+            int gotValue = sourceQueue->Dequeue(&value);
+            if (gotValue)
+            {
+                if (value % 2 == 0)
+                {
+                    evenQueue->Enqueue(&value);
+                }
+                else
+                {
+                    oddQueue->Enqueue(&value);
+                }
+            }
+            else if (evenQueue->GetSize() + oddQueue->GetSize() == size)
+            {
+                break;
+            }
+        }
     }
 
-    return true;
-}
+    static void* ProducerMethodStatic(void* arg)
+    {
+        ((Test*)arg)->ProducerMethod();
+        return NULL;
+    }
+
+    static void* ConsumerMethodStatic(void* arg)
+    {
+        ((Test*)arg)->ConsumerMethod();
+        return NULL;
+    }
+
+    bool RunThreads()
+    {
+        Thread producerThread(ProducerMethodStatic, this);
+        Thread consumer1Thread(ConsumerMethodStatic, this);
+        Thread consumer2Thread(ConsumerMethodStatic, this);
+
+        producerThread.Join();
+        consumer1Thread.Join();
+        consumer2Thread.Join();
+
+        return IsPassed();
+    }
+
+    bool IsPassed()
+    {
+        int evenSize = evenQueue->GetSize();
+        int oddSize = oddQueue->GetSize();
+
+        bool passed =
+            evenSize == (size_t)(size / 2) &&
+            oddSize == (size_t)(size / 2);
+
+        if (!passed)
+        {
+            return false;
+        }
+
+        int sum = 0;
+        int value = 0;
+
+        for (int i = 0; i < evenSize; i++)
+        {
+            evenQueue->Dequeue(&value);
+            sum += value;
+        }
+
+        for (int i = 0; i < oddSize; i++)
+        {
+            oddQueue->Dequeue(&value);
+            sum += value;
+        }
+
+        if (sum != (0 + size - 1) * size / 2) {
+            return false;
+        }
+
+        return true;
+    }
+};
 
 int main()
 {
-    int ok = Initialize();
-    if (! ok) {
-        goto Error;
+    Test test;
+
+    if (!test.IsOk()) {
+        return EXIT_FAILURE;
     }
 
-    RunThreads();
+    bool passed = test.RunThreads();
 
-    printf("=========== %s\n", IsPassed() ? "PASSED" : "FAILED");
+    printf("=========== %s\n", passed ? "PASSED" : "FAILED");
 
-    Release();
     return EXIT_SUCCESS;
-
-Error:
-    Release();
-    return EXIT_FAILURE;
 }
